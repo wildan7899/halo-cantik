6b8bc31a9427c6cb9d1ac44d6f0e113683ce431c
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-20 09:56:23 +0800

    Alpro

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    
+    int array
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string nama;
+    int a = 0;
+
+    while (a < 10) {
+        if (a % 2 == 0 ) {
+            cout << a << " ";
+        }
+        a++;
+    }
+
+    return 0;
+}
